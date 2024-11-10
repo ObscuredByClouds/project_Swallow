5802b941ca8ee97419db6940b0890961eee7fd29
pelmeskha <viktormedvedev12115551@gmail.com>
2024-11-10 12:38:16 +0300

    formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,19 @@
 #include <SFML/Graphics.hpp>
 
-   int main() {
-       sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Works!");
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Works!");
 
-       while (window.isOpen()) {
-           sf::Event event;
-           while (window.pollEvent(event)) {
-               if (event.type == sf::Event::Closed)
-                   window.close();
-           }
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
-           window.clear();
-           // Здесь вы можете рисовать свои объекты
-           window.display();
-       }
+        window.clear();
+        // Здесь вы можете рисовать свои объекты
+        window.display();
+    }
 
-       return 0;
+    return 0;
    }
