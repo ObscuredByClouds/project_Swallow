b4d23b1bb0390a9fdea3ca6fc2eaef5444a9dc34
Koln <korolyovkolian@gmail.com>
2024-11-17 19:12:08 +0300

    cmakelists, include memory

--- include/game_objects.hpp
+++ include/game_objects.hpp
@@ -4,6 +4,7 @@
 #include "constants.hpp"
 #include "controllers.hpp"
 #include "textures.hpp"
+#include <memory>
 
 //class Controller;  // Forward declaration
 
--- include/objects.hpp
+++ include/objects.hpp
@@ -2,6 +2,7 @@
 
 #include <SFML/Graphics.hpp>
 #include "constants.hpp"
+#include <memory>
 
 class Controller;
 
